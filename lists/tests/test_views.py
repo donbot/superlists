c9ed02a8f528a369905d7e38901a052a307d3326
donbot <donvhu@gmail.com>
2015-04-08 12:35:11 -0400

    enforce model validation in list view

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -85,12 +85,24 @@ class ListViewTest(TestCase):
     def test_POST_redirects_to_list_view(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
+        print("correct_id: " + str(correct_list.id))
 
         response = self.client.post(
             '/lists/%d/' % (correct_list.id,),
             data={'item_text': 'A new item for an existing list'})
 
         self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
+
+    def test_validation_errors_end_up_on_lists_page(self):
+        list_ = List.objects.create()
+        response = self.client.post(
+            '/lists/%d/' % (list_.id,),
+            data={'item_text': ''}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'list.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
 
 
 class HomePageTest(TestCase):
