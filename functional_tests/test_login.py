89666c140c431058ea36f8b34c9b244dc51c00c0
donbot <donvhu@gmail.com>
2015-04-15 09:12:04 -0400

    moved static outside repo. created accounts app

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -24,7 +24,7 @@ class LoginTest(FunctionalTest):
         # Edith goes to the awesome superlists site
         # and noticed a "Sign in" Link for the first time.
         self.browser.get(self.server_url)
-        self.browser.find_element_by_id('login').click()
+        self.browser.find_element_by_id('id_login').click()
 
         # A Persona Login box appears
         self.switch_to_new_window("Mozilla Persona")
@@ -40,6 +40,6 @@ class LoginTest(FunctionalTest):
         self.switch_to_new_window('To-Do')
 
         # She can see that she is Logged in
-        self.wait_for_element_with_id('logout')
+        self.wait_for_element_with_id('id_logout')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn('edith@mockmyid.com', navbar.text)
