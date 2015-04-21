b3ae0e2f391605591b08a8988cc559c0d5bd2b2b
donbot <donvhu@gmail.com>
2015-04-21 13:54:59 -0400

    fixed typo: check_all to check_call

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -14,7 +14,7 @@ def create_session_on_server(host, email):
 
 
 def reset_database(host):
-    subprocess.check_all(
+    subprocess.check_call(
         ['fab', 'reset_database', '--host={}'.format(host)],
         cwd=THIS_FOLDER
     )
