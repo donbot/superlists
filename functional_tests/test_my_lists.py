2ac8f3b0121c935c22cf8aea02f90a3413ed3abd
donbot <donvhu@gmail.com>
2015-04-23 16:15:19 -0400

    add list owners via forms. more isolated tests

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -8,63 +8,52 @@ from .base import FunctionalTest
 
 class MyListTest(FunctionalTest):
 
-	def create_pre_authenticated_session(self, email):
-		if self.against_staging:
-			session_key = create_session_on_server(self.server_host, email)
-		else:
-			session_key = create_pre_authenticated_session(email)
-		## to set a cookie we need to first visit the domain.
-		## 404 pages Load the quickest!
-		self.browser.get(self.server_url + "/404_no_such_url/")
-		self.browser.add_cookie(dict(
-			name=settings.SESSION_COOKIE_NAME,
-			value=session_key,
-			path='/',
-		))
+    def create_pre_authenticated_session(self, email):
+        if self.against_staging:
+            session_key = create_session_on_server(self.server_host, email)
+        else:
+            session_key = create_pre_authenticated_session(email)
+        ## to set a cookie we need to first visit the domain.
+        ## 404 pages Load the quickest!
+        self.browser.get(self.server_url + "/404_no_such_url/")
+        self.browser.add_cookie(dict(
+            name=settings.SESSION_COOKIE_NAME,
+            value=session_key,
+            path='/',
+        ))
 
-	def test_logged_in_users_lists_are_saved_as_my_lists(self):
-		email = 'edith@example.com'
 
-		self.browser.get(self.server_url)
-		self.wait_to_be_logged_out(email)
+    def test_logged_in_users_lists_are_saved_as_my_lists(self):
+        # Edith is a logged-in user
+        self.create_pre_authenticated_session('edith@example.com')
 
-		# Edith is a logged_in user
-		self.create_pre_authenticated_session(email)
+        # She goes to the home page and starts a list
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('Reticulate splines\n')
+        self.get_item_input_box().send_keys('Immanentize eschaton\n')
+        first_list_url = self.browser.current_url
 
-		self.browser.get(self.server_url)
-		self.wait_to_be_logged_in(email)
+        # She notices a "My lists" link, for the first time.
+        self.browser.find_element_by_link_text('My lists').click()
 
-	def test_logged_in_users_lists_are_saved_as_my_lists(self):
-		# Edith is a logged-in user
-		self.create_pre_authenticated_session('edith$example.com')
+        # She sees that her list is in there, named according to its 
+        # first list item
+        self.browser.find_element_by_link_text('Reticulate splines').click()
+        self.assertEqual(self.browser.current_url, first_list_url)
 
-		# She goes to the home page and starts a list
-		self.browser.get(self.server_url)
-		self.get_item_input_box().send_keys('Reticulate splines\n')
-		self.get_item_input_box().send_keys('Immunentize eschaton\n')
-		first_list_url = self.browser.current_url
+        # She decides to start another list, just to see
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('Click cows\n')
+        second_list_url = self.browser.current_url
 
-		# She notices a "My lists" link, for the first time.
-		self.browser.find_element_by_link_text('My lists').click()
+        # Under "My lists", her new list appears
+        self.browser.find_element_by_link_text('My lists').click()
+        self.browser.find_element_by_link_text('Click cows').click()
+        self.assertEqual(self.browser.current_url, second_list_url)
 
-		# She sees that her list is in there, named according to its 
-		# first list item
-		self.browser.find_element_by_link_text('Reticulate splines').click()
-		self.assertEqual(self.browser.current_url, first_list_url)
-
-		# She decides to start another list, just to see
-		self.browser.get(self.server_url)
-		self.get_item_input_box().send_keys('Click cows\n')
-		second_list_url = self.browser.current_url
-
-		# Under "My lists", her new list appears
-		self.browser.find_element_by_link_text('My lists').click()
-		self.browser.find_element_by_link_text('Click cows').click()
-		self.assertEqual(self.browser.current_url, second_list_url)
-
-		# She logs out. The "My lists" option disappears
-		self.browser.find_element_by_id('id_logout').click()
-		self.assertEqual(
-			self.browser.find_element_by_link_text('My lists'),
-			[]
-		)
+        # She logs out. The "My lists" option disappears
+        self.browser.find_element_by_id('id_logout').click()
+        self.assertEqual(
+            self.browser.find_elements_by_link_text('My lists'),
+            []
+        )
--- lists/views.py
+++ lists/views.py
@@ -3,7 +3,7 @@ from django.core.exceptions import ValidationError
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
-from lists.forms import ItemForm, ExistingListItemForm
+from lists.forms import ItemForm, ExistingListItemForm, NewListForm
 from lists.models import Item, List
 
 # Create your views here.
@@ -21,16 +21,14 @@ def view_list(request, list_id):
             return redirect(list_)
     return render(request, 'list.html', {'list': list_, 'form': form})
 
+
 def new_list(request):
-    form = ItemForm(data=request.POST)
+    form = NewListForm(data=request.POST)
     if form.is_valid():
-        list_ = List()
-        list_owner = request.user
-        list_.save()
-        form.save(for_list=list_)
+        list_ = form.save(owner=request.user)
         return redirect(list_)
-    else:
-        return render(request, 'home.html', {'form': form})
+    return render(request, 'home.html', {'form': form})
+
 
 def my_lists(request, email):
     owner = User.objects.get(email=email)
