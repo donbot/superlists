21443b16ed86d44e494726cfe0bc07d75eca8858
donbot <donvhu@gmail.com>
2015-04-24 14:37:39 -0400

    implicitly_wait given default value

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -40,7 +40,7 @@ class FunctionalTest(StaticLiveServerTestCase):
         if self.against_staging:
             reset_database(self.server_host)
         self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
+        self.browser.implicitly_wait(DEFAULT_WAIT)
 
     def tearDown(self):
         if self._test_has_failed():
